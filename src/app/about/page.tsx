9378fe4d491d4cb3449b16d9ddaab2f5f8898f85
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2023-11-22 15:52:30 -0300

    corrigindo a pagina

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function about() {
   return (
     <main>
       <h1>Sobre</h1>
+      <Link href="./">Home</Link>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Pagina inicial</h1>
+      <Link href="./about">Sobre</Link>
     </main>
   );
 }
