799f9aedc381167674106bc42779388474e9da7a
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2023-11-23 13:37:08 -0300

    teste 2

--- script/pullRequestPreview.mjs
+++ script/pullRequestPreview.mjs
@@ -65,6 +65,7 @@ fetch(`https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls`, {
     if (response.ok) return response.json();
     throw new Error(response.statusText);
   })
+  .then((response) => console.log(response))
   .catch((err) => {
     console.log("[COMMENT_ON_GITHUB: ERROR]");
     throw new Error(err);
